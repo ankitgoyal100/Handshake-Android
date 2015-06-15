1bec0606476a630ebace42cfb2b7a5bd03e0c225
Ankit Goyal <ankitgoyal100@gmail.com>
2015-06-14 21:57:36 -0400

    Synchronous user creation

--- app/src/main/java/com/handshake/Handshake/ContactServerSync.java
+++ app/src/main/java/com/handshake/Handshake/ContactServerSync.java
@@ -3,17 +3,14 @@ package com.handshake.Handshake;
 import android.content.Context;
 import android.os.Handler;
 
+import com.handshake.UserServerSync;
 import com.handshake.models.User;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 import org.apache.http.Header;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Iterator;
 
 import io.realm.Realm;
 import io.realm.RealmResults;
@@ -98,71 +95,11 @@ public class ContactServerSync {
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 System.out.println(response.toString());
 
-                Realm realm = Realm.getInstance(context);
-
-                final HashMap<Integer, JSONObject> map = new HashMap<Integer, JSONObject>();
-
                 try {
-                    JSONArray contacts = response.getJSONArray("contacts");
-                    for (int i = 0; i < contacts.length(); i++) {
-                        map.put(contacts.getJSONObject(i).getInt("id"), contacts.getJSONObject(i));
-                    }
+                    UserServerSync.addToQueue(response.getJSONArray("contacts"));
+                    UserServerSync.runQueue(context);
                 } catch (JSONException e) {
                     e.printStackTrace();
-                }
-
-                System.out.println("Map: " + map.size());
-
-                // update/delete contact and remove from list
-                Iterator it = map.keySet().iterator();
-                while (it.hasNext()) {
-                    final Integer key = (Integer) it.next();
-                    User result = realm.where(User.class).equalTo("userId", key).findFirst();
-                    if (result != null) {
-                        if (result.getSyncStatus() != Utils.userDeleted) {
-                            realm.beginTransaction();
-
-                            result = User.updateContact(result, realm, map.get(key));
-                            result.setSyncStatus(Utils.userSynced);
-                            try {
-                                result.setContactUpdated(Utils.formatDate(map.get(key).getString("contact_updated")));
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-
-                            realm.commitTransaction();
-
-                        }
-                        it.remove();
-                    }
-                }
-                System.out.println("Map after updating contacts: " + map.size());
-
-                // all left over are new contacts unless they are deleted
-                for (final int key : map.keySet()) {
-                    try {
-                        if (!map.get(key).getBoolean("is_contact")) {
-                            continue;
-                        }
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                    realm.beginTransaction();
-
-                    User user = realm.createObject(User.class);
-                    user = User.updateContact(user, realm, map.get(key));
-                    user.setSyncStatus(Utils.userSynced);
-                    try {
-                        user.setContactUpdated(Utils.formatDate(map.get(key).getString("contact_updated")));
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                    System.out.println("User added: " + user.toString());
-
-                    realm.commitTransaction();
                 }
 
                 try {
