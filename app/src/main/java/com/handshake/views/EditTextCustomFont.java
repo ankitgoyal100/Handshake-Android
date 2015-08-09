51bf7f5f0fae44bcae4d3e9168e28239e578337d
Ankit Goyal <ankitgoyal100@gmail.com>
2015-08-08 20:52:01 -0700

    Small fixes #3

--- app/src/main/java/com/handshake/views/EditTextCustomFont.java
+++ app/src/main/java/com/handshake/views/EditTextCustomFont.java
@@ -3,6 +3,10 @@ package com.handshake.views;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.view.inputmethod.InputConnectionWrapper;
 import android.widget.EditText;
 
 /**
@@ -29,6 +33,41 @@ public class EditTextCustomFont extends EditText {
         if (!isInEditMode()) {
             Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "helveticaneue.ttf");
             setTypeface(tf);
+        }
+    }
+
+    @Override
+    public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
+        return new EditTextInputConnection(super.onCreateInputConnection(outAttrs),
+                true);
+    }
+
+    private class EditTextInputConnection extends InputConnectionWrapper {
+
+        public EditTextInputConnection(InputConnection target, boolean mutable) {
+            super(target, mutable);
+        }
+
+        @Override
+        public boolean sendKeyEvent(KeyEvent event) {
+            if (event.getAction() == KeyEvent.ACTION_DOWN
+                    && event.getKeyCode() == KeyEvent.KEYCODE_DEL) {
+                // Un-comment if you wish to cancel the backspace:
+                // return false;
+            }
+            return super.sendKeyEvent(event);
+        }
+
+        @Override
+        public boolean deleteSurroundingText(int beforeLength, int afterLength) {
+            // magic: in latest Android, deleteSurroundingText(1, 0) will be called for backspace
+            if (beforeLength == 1 && afterLength == 0) {
+                // backspace
+                return sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL))
+                        && sendKeyEvent(new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_DEL));
+            }
+
+            return super.deleteSurroundingText(beforeLength, afterLength);
         }
     }
 }
